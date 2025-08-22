9889850f74545c1fbedfb8f7519a1298c976d1b5
Mathew Mouchamel <mathewmouchamel123@gmail.com>
2025-08-22 13:27:09 -0700

    import framer; convert h1 to motion h1

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,17 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="roboto-condensed-custom text-7xl">TaskFlowX</h1>
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center">
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="roboto-condensed-custom text-7xl"
+      >
+        TaskFlowX
+      </motion.h1>
     </div>
   );
 }
