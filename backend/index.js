44ec08d88e1fb802d4559993f3ccc569fe145e3e
Mathew Mouchamel <mathewmouchamel123@gmail.com>
2025-08-22 16:01:51 -0700

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
