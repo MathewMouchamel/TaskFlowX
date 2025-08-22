d9eb5dcc209fada7412c31b25f620b20d9bfe5c3
Mathew Mouchamel <mathewmouchamel123@gmail.com>
2025-08-22 13:28:37 -0700

    add delay to button

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <motion.button
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
         whileHover={{ scale: 1.08 }}
         whileTap={{ scale: 0.96 }}
         className="roboto-condensed-custom px-8 py-3 border border-white text-white text-2xl rounded-full transition-colors duration-200 cursor-pointer"
