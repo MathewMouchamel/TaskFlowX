80a6605e234fb38e3fd3fbce190fe850dfe63099
Mathew Mouchamel <mathewmouchamel123@gmail.com>
2025-08-22 12:56:36 -0700

    add title to homepage

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="roboto-condensed-custom text-7xl">TaskFlowX</h1>
+    </div>
+  );
 }
