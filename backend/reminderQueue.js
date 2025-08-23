aadcdf69fff695f554c9fc4ae59e39e12ce7fb30
Mathew Mouchamel <mathewmouchamel123@gmail.com>
2025-08-23 12:58:21 -0700

    modify schedule reminder function

--- backend/reminderQueue.js
+++ backend/reminderQueue.js
@@ -5,6 +5,6 @@ const connection = redisClient.duplicate();
 
 export const reminderQueue = new Queue("reminderQueue", { connection });
 
-export async function scheduleReminder({ userId, taskId, remindAt }) {
+export async function scheduleReminder({ userId, taskId }) {
   await reminderQueue.add("sendReminder", { userId, taskId });
 }
